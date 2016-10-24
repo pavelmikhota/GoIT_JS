d6c131493d6427dc060f4a182fe52a7f46ef86f6
Pavel Mikhota <pavelmikhota@live.ru>
2016-10-24 19:30:46 +0300

    add Exam

---
+++ exam/src/js/partials/_jcarousel.js
@@ -0,0 +1,49 @@
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function(e) {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function(e) {
+                $(this).removeClass('active');
+            })
+            .on('click', function(e) {
+                e.preventDefault();
+            })
+            .jcarouselPagination({
+                perPage: 1,
+                item: function(page) {
+                    return '<a href="#' + page + '">' + page + '</a>';
+                }});
+    });
+})(jQuery);
